6e6eb5b86b82458264dd708a04eeb10d9c2074b5
gitpranav17@gmail.com <gitpranav16@gmail.com>
2017-11-06 00:40:10 +0530

    Create files with problem statements for exer 20 and 21

---
+++ chapter1/exer1-21.c
@@ -0,0 +1,5 @@
+//Exercise 1-21
+//Write a program entab that replaces string of blanks by the minimum number of
+//tabs and blanks to achieve the same spacing. Use the same tabs stops as for detab.
+//When either a tab or a single blank would suffice to reach a tab stop,
+//which should be given preference ?
